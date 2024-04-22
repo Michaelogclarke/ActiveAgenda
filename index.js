c74884112d80844aa1b37dccd1a1bd3966ed125b
Michaelogclarke <128064847+Michaelogclarke@users.noreply.github.com>
2024-04-22 20:34:23 +0100

    pushing this as a safety net before I attempt mongoose

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const { MongoClient } = require("mongodb");
+const mongoose = require("mongooose");
 
 // Replace the uri string with your connection string.
-const uri = "<connection string uri>";
+const uri =
+  "mongodb+srv://michaelogclarke:Michaelog5106@activeagenda.dn3tprb.mongodb.net/?retryWrites=true&w=majority&appName=ActiveAgenda";
 
 const client = new MongoClient(uri);
 
 async function run() {
   try {
-    const database = client.db("sample_mflix");
-    const movies = database.collection("movies");
-
-    // Query for a movie that has the title 'Back to the Future'
-    const query = { title: "Back to the Future" };
-    const movie = await movies.findOne(query);
-
-    console.log(movie);
+    await mongoose.connect(uri);
+    console.log("Connected to Mongo");
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+
+connect();
 run().catch(console.dir);
