d0dabbae1939b6820df3d7e6bf984d5fadff93b4
Michaelogclarke <128064847+Michaelogclarke@users.noreply.github.com>
2024-04-23 03:00:13 +0100

    changed background, testing button ideas

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
 function addTask() {
-  if (inputBox.value === "") {
-    alert("You must write something!");
-  } else {
+  if (window.getComputedStyle(buttonElement).display === "block") {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
     let span = document.createElement("span");
     span.innerHTML = "\u00d7";
     li.appendChild(span);
+  } else {
+    alert("You must write something!");
   }
+
   inputBox.value = "";
   saveData();
 }
