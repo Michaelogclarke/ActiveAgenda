50520411147eafd1c1ac4bec5c9508abd82f0775
Michaelogclarke <128064847+Michaelogclarke@users.noreply.github.com>
2024-04-24 09:46:24 +0100

    Express setup

---
+++ server.js
@@ -0,0 +1,12 @@
+//express import
+const express = require("express");
+
+//express app
+const app = express();
+
+//Server start
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
