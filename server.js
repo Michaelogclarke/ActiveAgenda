c8420f86cddf58317be72308462b3a9622070f9e
Michaelogclarke <128064847+Michaelogclarke@users.noreply.github.com>
2024-04-25 09:08:25 +0100

    Server.js connecte & Mongoose install

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-//express import
+//import
 const express = require("express");
+const { MongoClient } = require("mongodb");
+
+//Mongodb connection
+const uri =
+  "mongodb+srv://michaelogclarke:michaelog5106@activeagenda.dn3tprb.mongodb.net/?retryWrites=true&w=majority&appName=ActiveAgenda"; // MongoDB connection URI
+const client = new MongoClient(uri);
+//Server connect for mongodb
+client.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MongoDB:", err);
+    return;
+  }
+  console.log("Connected to MongoDB");
+});
 
 //express app
 const app = express();
